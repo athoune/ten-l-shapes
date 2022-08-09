768ec11ede444a217d235baaf3e4da780e5b0341
Mathieu Lecarme <mathieu@garambrogne.net>
2022-08-09 17:55:08 +0200

    Show paths.

--- ten.py
+++ ten.py
@@ -3,7 +3,7 @@ jumps = {
     1: (6, 8),
     2: (7, 9),
     3: (4, 8),
-    4: (9, 3),
+    4: (0, 3, 9),
     5: (),
     6: (0, 1, 7),
     7: (2, 6),
@@ -14,14 +14,24 @@ jumps = {
 
 class L:
     jump: int
+    size: int
 
     def __init__(self, jump: int):
         self.jump = jump
+        self.size = 0
 
-    def start(self, position: int):
-        pass
+    def start(self, position: int, path=None):
+        if path is None:
+            path = [position]
+        if len(path) == self.jump:  # ok, jump quota is finished
+            self.size += 1
+            print(path)
+            return
+        for i in jumps[position]:  # next jumps
+            self.start(i, path + [i])
 
 
 if __name__ == "__main__":
     l = L(10)
     l.start(0)
+    print(l.size)
